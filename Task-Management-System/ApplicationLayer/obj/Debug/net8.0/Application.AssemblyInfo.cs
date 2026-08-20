48dbfeabe4d2721fe87168007c90a9230e9a645e
Əli Cabbarov <elicabbarov88@gmail.com>
2026-08-20 03:13:54 -0700

    Fix Tms Error

--- Task-Management-System/ApplicationLayer/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Task-Management-System/ApplicationLayer/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b36dfbe3e6641bc44410abf9b61e2973f58467e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83e086dcdeb7be1ca1e5cd3d88fc2f21c8a5755f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
